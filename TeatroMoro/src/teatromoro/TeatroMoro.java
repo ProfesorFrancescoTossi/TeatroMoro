cdf8f2a3a500529fd22e7f01c63ac1d38dee99a7
ProfesorFrancescoTossi <fr.tossi@profesor.duoc.cl>
2025-04-07 19:50:46 -0400

    primer commit

---
+++ TeatroMoro/src/teatromoro/TeatroMoro.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teatromoro;
+
+/**
+ *
+ * @author Fran
+ */
+public class TeatroMoro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code appsout   lication logic here
+        
+        System.out.println("Hola Teatro Moro!");
+    }
+    
+}
